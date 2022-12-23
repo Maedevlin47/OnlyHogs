d2b897d45a85cb78e84323890ead27128430baab
paige <paigetetz@gmail.com>
2022-12-23 10:15:31 -0500

    Changed titles to Pigstagram

--- my-app-frontend/src/components/HogPost.js
+++ my-app-frontend/src/components/HogPost.js
@@ -23,12 +23,12 @@ function HogPost ({addHog}) {
     }
     return (
         <div className='new-hog-form'>
-            <h2>NEW HOG</h2>
+            <h2>NEW PIG</h2>
             <form onSubmit = {handleSubmit}>
                 <input type="text" name="username" placeholder="Username" value = {username} onChange ={(e) => setUsername(e.target.value)}/>
                 <input type="text" name="image" placeholder="Image" value = {image} onChange ={(e) => setImage(e.target.value)}/>
                 <input type="text" name="caption" placeholder="Caption" value = {caption} onChange ={(e) => setCaption(e.target.value)}/>
-                <button type="submit" value="Add Pig" className="detail_button">ADD HOG</button>
+                <button type="submit" value="Add Pig" className="detail_button">POST PIG </button>
             </form>
         </div>
         
--- my-app-frontend/src/components/NavBar.js
+++ my-app-frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 
-import OnlyHogsLogo from "./OnlyHogsLogo.png";
+import Pigstagram from "./Pigstagram.png";
 import {NavLink} from 'react-router-dom'; 
 
 function NavBar() {
@@ -7,11 +7,11 @@ function NavBar() {
     return(
         <div className="nav_bar">  
             <div>
-                <img src={OnlyHogsLogo} alt="OnlyHogs Logo" className = "logo" />
+                <img src={Pigstagram} alt="Pigstagram Logo" className = "logo" />
             </div>
             <div className="nav_item">
                 <NavLink className="button" role="button" exact to ="/">HOME</NavLink>
-                <NavLink className="button" role="button" exact to ="/post">POST A HOG</NavLink>
+                <NavLink className="button" role="button" exact to ="/post">POST A PIG</NavLink>
             </div>
         </div>
     )
